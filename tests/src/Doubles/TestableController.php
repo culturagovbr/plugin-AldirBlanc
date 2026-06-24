d0d0c9c9b6fc7de32cbee44da585a9ae278d00bc
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2026-06-24 01:54:40 -0300

    test: adiciona double TestableController para sync e checkSyncStatus

--- tests/src/Doubles/TestableController.php
+++ tests/src/Doubles/TestableController.php
@@ -3,6 +3,8 @@
 namespace Tests\AldirBlanc\Doubles;
 
 use AldirBlanc\Controller;
+use AldirBlanc\Dtos\GestorDocument;
+use AldirBlanc\Jobs\GestorCultJob;
 
 /**
  * Expõe os métodos protected de Controller usados na normalização de ações do PAR
@@ -10,6 +12,80 @@ use AldirBlanc\Controller;
  */
 class TestableController extends Controller
 {
+    private string $gestorCpf = '12345678901';
+    private ?\Throwable $cpfException = null;
+    private $syncCallback = null;
+    private int $syncCalls = 0;
+
+    public function setGestorCpf(string $cpf): void
+    {
+        $this->gestorCpf = $cpf;
+    }
+
+    public function setCpfException(\Throwable $exception): void
+    {
+        $this->cpfException = $exception;
+    }
+
+    public function setSyncCallback(callable $callback): void
+    {
+        $this->syncCallback = $callback;
+    }
+
+    public function getSyncCalls(): int
+    {
+        return $this->syncCalls;
+    }
+
+    public function callStartSync(): void
+    {
+        $this->POST_startSync();
+    }
+
+    public function callCheckSyncStatus(): void
+    {
+        $this->GET_checkSyncStatus();
+    }
+
+    public function callLogoutOnError(): void
+    {
+        $this->POST_logoutOnError();
+    }
+
+    protected function getGestorCpf(): string
+    {
+        if ($this->cpfException) {
+            throw $this->cpfException;
+        }
+
+        return $this->gestorCpf;
+    }
+
+    protected function createGestorCultJob(GestorDocument $gestorDocument): GestorCultJob
+    {
+        $this->syncCalls++;
+        $callback = $this->syncCallback;
+
+        return new class($gestorDocument, $callback) extends GestorCultJob {
+            private $callback;
+
+            public function __construct(GestorDocument $gestorDocument, ?callable $callback)
+            {
+                parent::__construct($gestorDocument);
+                $this->callback = $callback;
+            }
+
+            public function sync(): bool
+            {
+                if ($this->callback) {
+                    return (bool) ($this->callback)();
+                }
+
+                return true;
+            }
+        };
+    }
+
     public function callRemoveDuplicatedParActions(array $actions): array
     {
         return $this->removeDuplicatedParActions($actions);
